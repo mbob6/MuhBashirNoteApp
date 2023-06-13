f08bb38b1d7d5d9d8dd40b96cab17e77dc11b5c7
Oni Bashir Muh'Bashir <108095967+Muhbashir2005@users.noreply.github.com>
2023-06-13 10:11:52 +0100

    Implemented Role Controller And View

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using NoteApp.Context;
+using NoteApp.Repository.Implementation;
+using NoteApp.Repository.Interfaces;
+using NoteApp.Services.Implementation;
+using NoteApp.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<NoteDbContext>(options => options.UseMySQL(builder.Configuration.GetConnectionString("NoteDbContext")));
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+builder.Services.AddScoped<IRoleService, RoleService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<INoteRepository, NoteRepository>();
+builder.Services.AddScoped<INoteService, NoteService>();
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
