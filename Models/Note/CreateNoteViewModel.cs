a68590b0f1115a453a63c11810f29d5cc68c6d25
Oni Bashir Muh'Bashir <108095967+Muhbashir2005@users.noreply.github.com>
2023-06-13 11:21:40 +0100

    fixing bug

--- Models/Note/CreateNoteViewModel.cs
+++ Models/Note/CreateNoteViewModel.cs
@@ -3,11 +3,7 @@
 namespace NoteApp.Models.Note;
 
 public class CreateNoteViewModel
-{
-
-    [Required(ErrorMessage = "Question text required")]
-    [MinLength(20, ErrorMessage = "Minimum of 20 character required")]
-    [MaxLength(150, ErrorMessage = "Maximum of 150 character required")]
+{ 
     public string Title { get; set; }
 
     public string Content { get; set; }
